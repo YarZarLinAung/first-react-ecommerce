a0012a15c32cde26a90e3431fb50c5d377101e83
yarzar-react <61220898+yarzar-react@users.noreply.github.com>
2020-02-19 16:02:18 +0630

    before adding route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage'
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hat Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/hats' component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
