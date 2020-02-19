688163bb6112eb6aa6e9361a7f301dc26d648451
yarzar-react <61220898+yarzar-react@users.noreply.github.com>
2020-02-19 13:09:14 +0630

    first commit
    Hompage finish

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './component/homepage'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
