141743b6e351c33abae44db0128a91afb4b2104b
yarzar-react <61220898+yarzar-react@users.noreply.github.com>
2020-02-28 14:18:02 +0630

    Finish Add to Cart
    Write cart item in cart list
    set duplicate for increasing quantity

--- src/components/cart/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart/cart-dropdown/cart-dropdown.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import CustomButton from '../../custom-button/custom-button'
 import './cart-dropdown.scss'
+import CartItem from '../cart-item/cart-item'
+import { connect } from 'react-redux'
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem=> 
+                    <CartItem 
+                        key={cartItem.id}
+                        item={cartItem}
+                        />)
+            }
+        </div>
         <CustomButton>Go To Checkout</CustomButton>
     </div>
 )
 
-export default CartDropDown
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropDown)
--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import CustomButton from '../../components/custom-button/custom-button'
 import './collection-item.scss'
+import { connect } from 'react-redux'
+import { addItem } from '../../redux/cart/cart-actions'
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className='collection-item'>
-        <div className="image"
-             style={{
-                 backgroundImage: `url(${imageUrl})`
-             }}
-        />
+const CollectionItem = ({item, formAddItem}) => {
+    const { name, price, imageUrl } = item
+    return(
+        <div className='collection-item'>
+            <div className="image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
 
-        <div className="collection-footer">
-             <span className='name'>{name}</span>
-             <span className='price'>{price}</span>
+            <div className="collection-footer">
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <CustomButton inverted onClick={() => {formAddItem(item)}}>Add to Cart</CustomButton>
         </div>
-        <CustomButton inverted>Add to Cart</CustomButton>
-    </div>
-)
+    )
+}
 
-export default CollectionItem
+const mapDispatchtoProps = dispatch => ({
+    formAddItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchtoProps)(CollectionItem)
